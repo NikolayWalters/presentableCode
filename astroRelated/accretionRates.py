cba5fe6ca7be1e35d375d7f6c1c14fef2ffcbb33
Nikolay Walters <zcapnwa@ucl.ac.uk>
2023-03-31 17:39:14 +0100

    Restructured the code

--- astroRelated/accretionRates.py
+++ astroRelated/accretionRates.py
@@ -38,15 +38,25 @@ M_cvz = (10**q_cvz)*M_WD
 tau = (10**log_tau)*u.yr
 accretionRate = X_Ca*M_cvz/tau
 print('Ca accretion rate ', accretionRate)
-print('Ca accretion rate ', accretionRate.to(u.g/u.second))
+print('Ca accretion rate ', accretionRate.to(u.g/u.second)) 
+orbitalSeparation = np.cbrt((M_BD+M_WD)*GravConstant*P_orb*P_orb/(4*np.pi*np.pi))
+orbitalSeparationWD = orbitalSeparation*M_BD/(M_WD+M_BD)
+v_orb = 2*np.pi*orbitalSeparationWD/P_orb # Keplerian velocity of the white dwarf
+orbitalSeparation = orbitalSeparation - R_BD
+print('Orbital separation: ',orbitalSeparation.to(u.R_sun))
 
-orbitalSeparation = np.cbrt((M_BD+M_WD)*GravConstant*P_orb*P_orb/(4*np.pi*np.pi))
-orbitalSeparation = orbitalSeparation*M_BD/(M_WD+M_BD)
-v_orb = 2*np.pi*orbitalSeparation/P_orb # km/s keplerian velocity of the white dwarf
-print('Orbital separation: ',orbitalSeparation.to(u.au))
-orbitalSeparation = np.cbrt((M_BD+M_WD)*GravConstant*P_orb*P_orb/(4*np.pi*np.pi))
-orbSep = orbitalSeparation - R_BD
-print('Orbital separation: ',orbSep.to(u.R_sun))
+# accretion calculations
+
+#elemental abundances
+atomicMasses = [element.mass for element in periodictable.elements]
+solarAbundances = [2.884e10,2.288e9,55.47,0.7374,\
+17.32,7.079e6,1.95e6,1.413e7,841.1,2.148e6,5.751e4,\
+1.021e6,8.410e4,1e6,8373,4.449e5,5237,1.025e5,3692,\
+6.287e4,34.20,2422,288.4,1.286e4,9168,8.380e5,2323,\
+4.780e4,527,1226,35.97,120.6,6.089,65.79,11.32,55.15,\
+6.572,23.64,4.608,11.33,0.7554,2.601,1.9,1.435,1.584,\
+3.733,4.815,5.391,4.351,1.169,1.357,3.258]
+
 totalMass = 2.884e10*1.00794+2.288e9*4.002602+55.47*6.941+0.7374*9.012182+\
 17.32*10.811+7.079e6*12.0107+1.95e6*14.0067+1.413e7*15.9994+\
 841.1*18.9984032+2.148e6*20.1797+5.751e4*22.98976928+1.021e6*24.3050+\
@@ -69,22 +79,22 @@ print('Total accreted mass: ', MDot.to(u.g/u.second))
 
 # bondi hoyle
 velocityAcc = np.sqrt(v_escape*v_escape+v_orb*v_orb)
-BD_massLoss = (MDot*orbSep*orbSep*(velocityAcc**4))/(GravConstant*GravConstant*M_WD*M_WD)
+BD_massLoss = (MDot*orbitalSeparation*orbitalSeparation*(velocityAcc**4))/(GravConstant*GravConstant*M_WD*M_WD)
 
 print('Bondi Hoyle ', BD_massLoss.to(u.g/u.second))
 
 print('Bondi Hoyle ', BD_massLoss.to(u.M_sun/u.yr))
 BH_val = 5.1e-17*(u.M_sun/u.yr)
-print('Rho_BH', (BH_val/(4*np.pi*velocityAcc*orbSep*orbSep)).to(u.M_sun/u.R_sun/u.R_sun/u.R_sun))
+print('Rho_BH', (BH_val/(4*np.pi*velocityAcc*orbitalSeparation*orbitalSeparation)).to(u.M_sun/u.R_sun/u.R_sun/u.R_sun))
 
 
 # gravitational
 velocityAcc = np.sqrt(v_escape*v_escape+v_orb*v_orb)
 
-BD_massLoss = (2*MDot*velocityAcc*velocityAcc*orbSep*orbSep)/(GravConstant*M_WD*R_WD)
+BD_massLoss = (2*MDot*velocityAcc*velocityAcc*orbitalSeparation*orbitalSeparation)/(GravConstant*M_WD*R_WD)
 
 print('Gravitational ', BD_massLoss.to(u.g/u.second))
 
 print('Gravitational ', BD_massLoss.to(u.M_sun/u.yr))
 Edd_val = 2.0e-15*(u.M_sun/u.yr)
-print('Rho_Edd', (Edd_val/(4*np.pi*velocityAcc*orbSep*orbSep)).to(u.M_sun/u.R_sun/u.R_sun/u.R_sun))
+print('Rho_Edd', (Edd_val/(4*np.pi*velocityAcc*orbitalSeparation*orbitalSeparation)).to(u.M_sun/u.R_sun/u.R_sun/u.R_sun))
