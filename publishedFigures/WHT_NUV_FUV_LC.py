d22da883e6d9b959fb243ded0e1e0e6eab0aa935
Nikolay Walters <zcapnwa@ucl.ac.uk>
2023-06-12 21:05:05 +0100

    Added comments and cleaned up code

--- publishedFigures/WHT_NUV_FUV_LC.py
+++ publishedFigures/WHT_NUV_FUV_LC.py
@@ -1,3 +1,9 @@
+"""
+A script to plot 3 phase folded light curves of GD 356
+from WHT u-band, HST NUV and FUV observations. The script
+also includes a best-fit line plots.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 from lmfit import Parameters, minimize, report_fit, Model,fit_report
@@ -13,6 +19,9 @@ rcParams['font.sans-serif'] = ['Times']
 
 
 def foldMeth(timeArray, period):
+    """
+    Method to convert between time and phase for a specified period
+    """
     phases = []
     for el in timeArray:
         passed = int(el/period)
@@ -22,61 +31,54 @@ def foldMeth(timeArray, period):
     return np.array(phases)
 
 def sineFun(x,amp,phase,off):
+    """
+    A simple sine function used as a best fit line for the data
+    The frequency is hardcoded to 2*pi because the lightcurves
+    are already phase folded
+    """
     return amp * np.sin(6.283185307*x + phase)+off
 
+# reading in WHT u-band data
 indata = np.loadtxt("GD356Uband.dat", usecols=(0,1))
 timeWHTU = indata[:,0]
 fluxWHTU = indata[:,1]
 
+# reading in HST FUV data
 indata = np.loadtxt("HSTPhotFUVAnother.dat", usecols=(0,1))
 timeHSTFUV = indata[:,0] +2457000
 fluxHSTFUV = indata[:,1]
 
-
+# reading in HST NUV data
 indata = np.loadtxt("HSTPhotNUV.dat", usecols=(0,1))
 timeHSTNUV = indata[:,0]
 fluxHSTNUV = indata[:,1]
 
-
-
-
-
-
-
+# defining global plot properties
 rc('axes', linewidth=2)
 fig, axs = plt.subplots(3, 1,
                         gridspec_kw={'hspace': 0., 'wspace': 0}, figsize=(20,30))
+
+# creating a u-band phase fold with re-binnning
 timeWHTUAdj = timeWHTU-2457000.- 1296.0255697684468
 WHTPhases = foldMeth(timeWHTUAdj,  0.08029820145279275)
 sortIndi = np.argsort(WHTPhases)
 WHTPhases = WHTPhases[sortIndi]
 fluxWHTUS = fluxWHTU[sortIndi]
-
 meanPoints = 5
 fluxWHTUS = np.nanmean(np.pad(fluxWHTUS.astype(float), (0, meanPoints - fluxWHTUS.size%meanPoints), mode='constant', constant_values=np.NaN).reshape(-1, meanPoints), axis=1)
 WHTPhasesMean = np.nanmean(np.pad(WHTPhases.astype(float), (0, meanPoints - WHTPhases.size%meanPoints), mode='constant', constant_values=np.NaN).reshape(-1, meanPoints), axis=1)
-axs[0].plot(WHTPhasesMean, fluxWHTUS-1, c='g',marker='o', ls = '')
-axs[0].plot(WHTPhasesMean+1, fluxWHTUS-1, c='g', marker='o', ls = '')
-axs[0].plot(WHTPhasesMean, sineFun(WHTPhasesMean,0.01569, 1.828,0), c='k',lw=4)
-axs[0].plot(WHTPhasesMean+1, sineFun(WHTPhasesMean,0.01569, 1.828,0), c='k',lw=4)
 
-
+# and then NUV phase fold
 timeHSTNUVAdj = timeHSTNUV-2457000- 1296.0255697684468
 PhaseHSTNUV = foldMeth(timeHSTNUVAdj,  0.08029820145279275)
 sortIndi = np.argsort(PhaseHSTNUV)
 PhaseHSTNUV = PhaseHSTNUV[sortIndi]
 fluxHSTNUVAdj = fluxHSTNUV[sortIndi]
-
 meanPoints = 5
 fluxHSTNUVAdj = np.nanmean(np.pad(fluxHSTNUVAdj.astype(float), (0, meanPoints - fluxHSTNUVAdj.size%meanPoints), mode='constant', constant_values=np.NaN).reshape(-1, meanPoints), axis=1)
 PhaseHSTNUV = np.nanmean(np.pad(PhaseHSTNUV.astype(float), (0, meanPoints - PhaseHSTNUV.size%meanPoints), mode='constant', constant_values=np.NaN).reshape(-1, meanPoints), axis=1)
-axs[1].plot(PhaseHSTNUV, fluxHSTNUVAdj, c='b',marker='o', ls = '')
-axs[1].plot(PhaseHSTNUV+1, fluxHSTNUVAdj, c='b', marker='o', ls = '')
-axs[1].plot(PhaseHSTNUV, sineFun(PhaseHSTNUV,0.02803, 1.566,0), c='k',lw=4)
-axs[1].plot(PhaseHSTNUV+1, sineFun(PhaseHSTNUV,0.02803, 1.566,0), c='k',lw=4)
 
-
-
+# and finally FUV
 timeHSTFUVAdj = np.array(timeHSTFUV)-2457000 - 1296.0255697684468
 phaseHSTFUV = foldMeth(timeHSTFUVAdj,  0.08029820145279275)
 sortIndi = np.argsort(phaseHSTFUV)
@@ -85,13 +87,23 @@ fluxHSTFUVAdj = fluxHSTFUV[sortIndi]
 meanPoints = 5
 fluxHSTFUVAdj = np.nanmean(np.pad(fluxHSTFUVAdj.astype(float), (0, meanPoints - fluxHSTFUVAdj.size%meanPoints), mode='constant', constant_values=np.NaN).reshape(-1, meanPoints), axis=1)
 PhaseHSTFUV = np.nanmean(np.pad(phaseHSTFUV.astype(float), (0, meanPoints - phaseHSTFUV.size%meanPoints), mode='constant', constant_values=np.NaN).reshape(-1, meanPoints), axis=1)
+
+# plotting phase folded LCs and best-fit lines
+axs[0].plot(WHTPhasesMean, fluxWHTUS-1, c='g',marker='o', ls = '')
+axs[0].plot(WHTPhasesMean+1, fluxWHTUS-1, c='g', marker='o', ls = '')
+axs[0].plot(WHTPhasesMean, sineFun(WHTPhasesMean,0.01569, 1.828,0), c='k',lw=4)
+axs[0].plot(WHTPhasesMean+1, sineFun(WHTPhasesMean,0.01569, 1.828,0), c='k',lw=4)
+axs[1].plot(PhaseHSTNUV, fluxHSTNUVAdj, c='b',marker='o', ls = '')
+axs[1].plot(PhaseHSTNUV+1, fluxHSTNUVAdj, c='b', marker='o', ls = '')
+axs[1].plot(PhaseHSTNUV, sineFun(PhaseHSTNUV,0.02803, 1.566,0), c='k',lw=4)
+axs[1].plot(PhaseHSTNUV+1, sineFun(PhaseHSTNUV,0.02803, 1.566,0), c='k',lw=4)
 axs[2].plot(PhaseHSTFUV, fluxHSTFUVAdj, c='magenta',marker='o', ls = '')
 axs[2].plot(PhaseHSTFUV+1, fluxHSTFUVAdj, c='magenta', marker='o', ls = '')
 axs[2].plot(PhaseHSTFUV, sineFun(PhaseHSTFUV,0.09397, 1.552,0), c='k',lw=4)
 axs[2].plot(PhaseHSTFUV+1, sineFun(PhaseHSTFUV,0.09397, 1.552,0), c='k',lw=4)
 
 
-
+# setting further plot properties and saving the plot
 xlimits = axs[0].set_xlim()
 axs[0].set_xlim(xlimits)
 axs[2].set_xlabel("Phase", fontsize=36)
